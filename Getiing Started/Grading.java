220b8a07c2a8d83d7a547e445ade72713440d2a4
kaustubhnegi <kaustubhnegi1@gmail.com>
2022-03-10 12:16:04 +0530

    grading

--- Getiing Started/Grading.java
+++ Getiing Started/Grading.java
@@ -1,5 +1,11 @@
 package Getiing Started;
 
-public class Grading {
+import java.util.*;
+public class Main {
+    public static void main (String args[]){
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+    }
     
 }
